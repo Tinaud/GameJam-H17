5da4513ef006d4f85b7c06ab503adfb872517043
Tinaud <tinaud1415@gmail.com>
2017-02-21 15:30:38 -0500

    Premier petit Script pour tester

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private bool canMove;
+	private float moveSpeed;
+
+	void Start () {
+		canMove = true;
+		moveSpeed = 0.5f;
+	}
+	
+	void Update () {
+		if(canMove)
+			transform.Translate(Vector3.up *  moveSpeed * Time.deltaTime);
+
+		if(Input.GetKeyDown("space"))
+			canMove = !canMove;
+	}
+}
