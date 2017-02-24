45f0d41359468e318e1d7d1711aafd53386bdb9c
Patrick Levesque <levesque_patrick_2@hotmail.com>
2017-02-24 17:33:50 -0500

    Character Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float moveSpeed;
+    private float horizontalSpeed, verticalSpeed;
+    private bool action;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontalSpeed = Input.GetAxis("Horizontal");
+        verticalSpeed = Input.GetAxis("Vertical");
+        //action = Input.GetKeyDown("JoystickButton1");
+
+        if(horizontalSpeed > 0)
+        {
+            transform.Translate(Vector3.right * horizontalSpeed * moveSpeed * Time.deltaTime);
+        }
+
+        if (horizontalSpeed < 0)
+        {
+            transform.Translate(Vector3.left * -horizontalSpeed * moveSpeed * Time.deltaTime);
+        }
+
+        if (verticalSpeed > 0)
+        {
+            transform.Translate(Vector3.up * verticalSpeed * moveSpeed * Time.deltaTime);
+        }
+
+        if (verticalSpeed < 0)
+        {
+            transform.Translate(Vector3.down * -verticalSpeed * moveSpeed * Time.deltaTime);
+        }
+    }
+}
