c5bc1a488d38c52ba6e20737917cca3234d30aca
pyglidden <pierreyves.glidden@hotmail.com>
2017-02-25 01:51:15 -0500

    bullet collision

---
+++ Assets/Scripts/BulletKill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletKill : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(gameObject.tag) || !other.CompareTag("Wall"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
