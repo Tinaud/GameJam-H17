779c21a28ee0dab5dd8506b6a4fe3c9837dbe2b8
pyglidden <pierreyves.glidden@hotmail.com>
2017-02-24 23:06:38 -0500

    more tweet + acces au type de sujet

--- Assets/Scripts/TweetScripter.cs
+++ Assets/Scripts/TweetScripter.cs
@@ -8,6 +8,7 @@ public class TweetScripter : MonoBehaviour {
     public float startTweetWait;
     public float afterTweetWait;
     private string tweet;
+    private int subject;
     public GUIText tweetText;
     public GameObject tweetWindow;
 
@@ -20,16 +21,15 @@ public class TweetScripter : MonoBehaviour {
             yield return new WaitForSeconds(frequencyTweet);
             if(Random.Range(0,10) == 3)
             {
-                int subject = 0;
                 tweetWindow.SetActive(true);
                 int numTweet = 0;
                 subject = Random.Range(0, 2);
 
-                //subject:    0-Random subject, no particular bonuses 1-Woman 2-Man
+                //subject:    0-Random subject, no particular bonuses 1-Woman 2-Man 3-Trump is happy about what you do 4-Trump is not happy about what you do
                 switch (subject)
                 {
                     case 0:
-                        numTweet = Random.Range(0, 2);
+                        numTweet = Random.Range(0, 3);
                         switch (numTweet)
                         {
                             case 0:
@@ -39,7 +39,7 @@ public class TweetScripter : MonoBehaviour {
                                 tweet = "Big interview tonight at Fox News";
                                 break;
                             case 2:
-                                tweet = "www.pornhub.com\nDon't botter the last tweet! i thought i was on google.";
+                                tweet = "www.pornhub.com/trump\nDon't botter the last tweet! i thought i was on google.";
                                 break;
                             case 3:
                                 tweet = "Screw the other countries...";
@@ -60,7 +60,7 @@ public class TweetScripter : MonoBehaviour {
                                 tweet = "Woman should not have the rigth to vote, they are dumb.";
                                 break;
                             case 3:
-                                tweet = "Most of women are stupid, cheaters, liars, can't think right.\n Other than that their body are great.";
+                                tweet = "Most of women are stupid, cheaters, liars, can't think right.\nOther than that their body are great.";
                                 break;
                             case 4:
                                 tweet = "Must be a pretty picture mexican's women dropping on their knees";
@@ -85,6 +85,39 @@ public class TweetScripter : MonoBehaviour {
                                 break;
                         }
                         break;
+                    case 3:
+                        numTweet = Random.Range(0, 3);
+                        switch (numTweet)
+                        {
+                            case 0:
+                                tweet = "The U.S. military are doing a great job at the mexican border!";
+                                break;
+                            case 1:
+                                tweet = "We are intercepting a lot of mexican illegal immigrants";
+                                break;
+                            case 2:
+                                tweet = "The wall is getting bigger!";
+                                break;
+                            case 3:
+                                tweet = "100 000 000 $ military budget as been established for \nthe mexican border";
+                                break;
+                        }
+                        break;
+                    case 4:
+                        numTweet = Random.Range(0, 2);
+                        switch (numTweet)
+                        {
+                            case 0:
+                                tweet = "The General of 'the operation illegal immegrant' is an incapable";
+                                break;
+                            case 1:
+                                tweet = "Come on troups! get rid of those mexicans!";
+                                break;
+                            case 2:
+                                tweet = "My grandmother could be more efficient than the custom officers";
+                                break;
+                        }
+                        break;
                 }
                 tweetText.text = tweet;
                 yield return new WaitForSeconds(afterTweetWait);
@@ -93,9 +126,15 @@ public class TweetScripter : MonoBehaviour {
         }
     }
 
+    public int tweetSubject()
+    {
+        return subject;
+    }
+
     // Use this for initialization
     void Start () {
         tweet = "";
+        subject = 0;
         StartCoroutine(SpawnTweet());
         tweetWindow.SetActive(false);
     }
